4d3f38af69d30e9ff605e7c0fa9a34954184a66d
Marco Caloba <marco_caloba@hotmail.com>
2023-11-27 21:10:10 +0000

    Initial commit

---
+++ traffic_monitoring_api/urls.py
@@ -0,0 +1,44 @@
+"""
+URL configuration for traffic_monitoring_api project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+# Basic schema for the Swagger documentation
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Traffic Monitoring",
+        default_version='v1',
+        contact=openapi.Contact(email="marco_caloba@hotmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('traffic_api.urls')),
+    
+    # URL's for the Swagger documentation
+    re_path(r'^api/docs(?P<format>\\.json|\\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/docs/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
